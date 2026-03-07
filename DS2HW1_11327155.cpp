2d713f366c880962fc0d3094b47485d1c073d682
kitakaaki <kellenhuang168@gmail.com>
2026-03-07 17:52:08 +0800

    3/7 17:52 update

--- DS2HW1_11327155.cpp
+++ DS2HW1_11327155.cpp
@@ -29,6 +29,9 @@
 輸出：分別顯示前述三個節點對應的（序號、上學年度畢業生數）於螢幕上。*/
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <cmath>
 
 struct Data {
     std::string schoolCode; // 學校代碼
@@ -42,6 +45,7 @@ struct Data {
     int graduates; // 上學年度畢業生數
     std::string city; // 縣市名稱
     std::string system; // 體系別
+    int index; // 序號(後產生)
 };
 
 class MaxHeap {
@@ -51,10 +55,10 @@ class MaxHeap {
   public:
     void insert(int index, int graduates) {
       heap.push_back(std::make_pair(index, graduates));
-      int i = heap.size() - 1;
-      while (i > 0 && heap[i].second > heap[(i - 1) / 2].second) {
-        std::swap(heap[i], heap[(i - 1) / 2]);
-        i = (i - 1) / 2;
+      int pos = heap.size() - 1;
+      while (pos > 0 && heap[pos].second > heap[(pos - 1) / 2].second) {
+        std::swap(heap[pos], heap[(pos - 1) / 2]);
+        pos = (pos - 1) / 2;
       }
     }
 
@@ -84,8 +88,75 @@ class DEAP {
   private:
     std::vector<std::pair<int, int>> deap; // (序號, 上學年度畢業生數)
 
+    bool isInMinHeap(int pos) {
+      int posLevel = std::floor(std::log2(pos + 1));
+      int leftOfMaxHeap = std::pow(2, posLevel - 1) * 3 - 1;
+      return pos < leftOfMaxHeap;
+    }
+
+    void ReheapUpMin(int pos) {
+      while (pos > 0 && deap[pos].second < deap[(pos - 1) / 2].second) {
+        std::swap(deap[pos], deap[(pos - 1) / 2]);
+        pos = (pos - 1) / 2;
+      }
+    }
+
+    void ReheapUpMax(int pos) {
+      while (pos > 0 && deap[pos].second > deap[(pos - 1) / 2].second) {
+        std::swap(deap[pos], deap[(pos - 1) / 2]);
+        pos = (pos - 1) / 2;
+      }
+    }
+
+    int findCorrespondingNode(int pos) {
+      int posLevel = std::floor(std::log2(pos + 1));
+      int offset = std::pow(2, posLevel - 1);
+      int corPos;
+      if (isInMinHeap(pos)) {
+        corPos = pos + offset;
+      } else {
+        corPos = pos - offset;
+      }
+      while (corPos >= deap.size()) {
+        corPos = (corPos - 1) / 2;
+      }
+      return corPos;
+    } 
   public:
-    void insert(int index, int graduates) {}
+    DEAP() {
+      deap.push_back(std::make_pair(0, -1)); // index: 0
+    }
+
+    void insert(int index, int graduates) {
+      deap.push_back(std::make_pair(index, graduates));
+      int pos = deap.size() - 1;
+      int corPos = findCorrespondingNode(pos);
+      //  Examine the corresponding nodes
+      if (isInMinHeap(pos)) {
+        if (deap[pos].second > deap[corPos].second) {
+          std::swap(deap[pos], deap[corPos]);
+          pos = corPos;
+        }
+      } else {
+        if (deap[pos].second < deap[corPos].second) {
+          std::swap(deap[pos], deap[corPos]);
+          pos = corPos;
+        }
+      }
+      // ReheapUp if necessary
+      if (isInMinHeap(pos)) {
+        ReheapUpMin(pos);
+      } else {
+        ReheapUpMax(pos);
+      }
+    }
+
+     std::pair<int, int> getMax() {
+      if (!deap.empty()) {
+        return deap[0];
+      }
+      return std::make_pair(-1, -1);
+    }
 
     std::pair<int, int> getBottom() {
       if (!deap.empty()) {
@@ -104,36 +175,124 @@ class DEAP {
 
 class IO {
   public:
-    static std::vector<Data> readData(const std::string& filename) {}
+    static std::vector<Data> readData(const std::string& filenumber) {  
+      std::vector<Data> dataList;
+      std::string filename = "input" + filenumber + ".txt";
+      std::ifstream file(filename);
+      if (!file.is_open()) 
+        return dataList;
+      std::string header;
+      for (int i = 0; i < 3; ++i) {
+        std::getline(file, header);
+      }
+      std::string line;
+      int index = 1;
+      while (std::getline(file, line)) {
+        Data data;
+        std::istringstream linestream(line);
+        std::getline(linestream, data.schoolCode, '\t');
+        std::getline(linestream, data.schoolName, '\t');
+        std::getline(linestream, data.departmentCode, '\t');
+        std::getline(linestream, data.departmentName, '\t');
+        std::getline(linestream, data.dayOrNight, '\t');
+        std::getline(linestream, data.level, '\t');
+        linestream >> data.students >> data.teachers >> data.graduates;
+        linestream.ignore(); // 跳過graduates後的tab
+        std::getline(linestream, data.city, '\t');
+        std::getline(linestream, data.system);
+        data.index = index++;
+        dataList.push_back(data);
+      }
+      return dataList;
+    }
 
     static void printNode(const std::pair<int, int>& node) {
       if (node.first != -1) {
-        std::cout <<  "id: " << node.first << ", graduates: " << node.second << std::endl;
+        std::cout <<  "[" << node.first << "] " << node.second << std::endl;
       } else {
         std::cout << "no such node!" << std::endl;
       }
     }
 };
 
-int main() {
-    while (1) {
-        std::cout << "* Data Structures and Algorithms *" << std::endl;
-        std::cout << "* 0. Exit                        *" << std::endl;
-        std::cout << "* 1. Create max heap             *" << std::endl;
-        std::cout << "* 2. Create DEAP                 *" << std::endl;
-        std::cout << "Please enter command: ";
-        std::string command;
-        std::cin >> command;
-
-        if (command == "0") {
-            break;
-        } else if (command == "1") {
-            // 執行任務一
-        } else if (command == "2") {
-            // 執行任務二
+class system {
+  private:
+    static void mission1() {
+      std::string filenumber;
+      std::vector<Data> dataList = IO::readData(filenumber);
+      while (true) {
+        std::cout << std::endl << "Input a file number ([0] Quit): ";
+        std::cin >> filenumber;
+        dataList = IO::readData(filenumber);
+        if (dataList.empty()) {
+          std::cout << std::endl << "### input" << filenumber << ".txt does not exist! ###" << std::endl;
         } else {
-            std::cout << "Invalid command. Please try again." << std::endl;
+          break;
         }
+      }
+      MaxHeap maxHeap;
+      for (const auto& data : dataList) {
+        maxHeap.insert(data.index, data.graduates);
+      }
+      std::cout << "<max heap>" << std::endl << "root: ";
+      IO::printNode(maxHeap.getMax());
+      std::cout << "bottom: ";
+      IO::printNode(maxHeap.getBottom());
+      std::cout << "leftmost bottom: ";
+      IO::printNode(maxHeap.getLeftBottom());
+      std::cout << std::endl;
     }
+
+    static void mission2() {
+      std::string filenumber;
+      std::vector<Data> dataList = IO::readData(filenumber);
+      while (true) {
+        std::cout << std::endl << "Input a file number ([0] Quit): ";
+        std::cin >> filenumber;
+        dataList = IO::readData(filenumber);
+        if (dataList.empty()) {
+          std::cout << std::endl << "### input" << filenumber << ".txt does not exist! ###" << std::endl;
+        } else {
+          break;
+        }
+      }
+      DEAP deap;
+      for (const auto& data : dataList) {
+        deap.insert(data.index, data.graduates);
+      }
+      std::cout << "<DEAP>" << std::endl << "bottom: ";
+      IO::printNode(deap.getBottom());
+      std::cout << "leftmost bottom: ";
+      IO::printNode(deap.getLeftBottom());
+      std::cout << std::endl;
+    }
+
+  public:
+    static void run() {
+      while (true) {
+        std::cout << "* Data Structures and Algorithms *" << std::endl;
+        std::cout << "*** Heap Construction and Use ****" << std::endl;
+        std::cout << "* 0. QUIT                        *" << std::endl;
+        std::cout << "* 1. Build a max heap            *" << std::endl;
+        std::cout << "* 2. Build a DEAP                *" << std::endl;
+        std::cout << "***************************** ****" << std::endl;
+        std::cout << "Input a choice(0, 1, 2): ";
+        std::string choice;
+        std::cin >> choice;
+        if (choice == "0") {
+          break;
+        } else if (choice == "1") {
+          mission1();
+        } else if (choice == "2") {
+          mission2();
+        } else {
+          std::cout << std::endl << "Command does not exist!" << std::endl;
+        }
+      }
+    }
+};
+
+int main() {
+    system::run();
     return 0;
-}
+} 
