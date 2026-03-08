63a7325121187ad65c372f1570c8226b726344cb
kitakaaki <kellenhuang168@gmail.com>
2026-03-08 17:20:22 +0800

    3/8 17:20 update (mission 1, 2 complete)

--- DS2HW1_11327155.cpp
+++ DS2HW1_11327155.cpp
@@ -1,32 +1,4 @@
 /*11327155 黃宇謙*/
-/*二、作業內容
-資料檔格式簡述：
-(1) 取自教育部統計處的大學校系畢業生資訊，前三列為標題和欄名，第四列起為各校系某
-班制的畢業生資訊，共 11 個欄位，以定位符號（'/t'）間隔，依「學校代碼」遞增排序。
-(2) 欄位由左至右依序為：學校代碼、學校名稱、科系代碼、科系名稱、日間∕進修別、等
-級別、學生數、教師數、上學年度畢業生數、縣市名稱、體系別，資料檔名如 input101.txt。
-(3) 按照原始的存檔次序為每筆資料附上從 1 號開始的唯一『序號』。
-規範：每個任務違反一項各扣 5 分
-(1) 預先不知道資料筆數，禁止使用固定筆數的靜態陣列，必須採用動態陣列或 vector。
-(2) 必須為主要資料結構定義專屬的 C++類別，至少要有二種指定堆積專用的類別
-整合下列任務於單一程式，並遵循範例程式的輸入輸出介面，未整合、無法連續執行或沒有
-輸入防呆措施，都各扣 5 分。若導致程式檢測系統無法正常運作，該任務以零分計。
-必須(而且只限)出現在任務選單第一列：* Data Structures and Algorithms *
-（任務一）建立最大堆積 max heap
-輸入：原始資料檔，資料檔名如 input101.txt。
-步驟：
-(1) 每筆資料附上唯一序號後，以『上學年度畢業生數』建立一棵最大堆積，依照序號由小
-到大一筆一筆新增至原有的堆積結構中，每個節點只存放一筆資料對應的（序號、上學
-年度畢業生數）。
-(2) 找到最大堆積的樹根，底部節點（bottom），以及最左下角的節點。
-輸出：分別顯示前述三個節點對應的（序號、上學年度畢業生數）於螢幕上。
-（任務二）建立雙堆積 DEAP
-輸入：原始資料檔，資料檔名如 input101.txt。
-步驟：
-(1) 以『上學年度畢業生數』再建立一棵雙堆積，依照任務一所附加的序號由小到大逐筆新
-增至原有的堆積結構，每個節點存放一筆資料對應的（序號、上學年度畢業生數）。
-(2) 找到雙堆積的（右下角）底部節點（bottom）及最左下角的節點。
-輸出：分別顯示前述三個節點對應的（序號、上學年度畢業生數）於螢幕上。*/
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -99,14 +71,14 @@ class DEAP {
     }
 
     void ReheapUpMin(int pos) {
-      while (pos > 0 && deap[pos].second < deap[(pos - 1) / 2].second) {
+      while (pos > 1 && deap[pos].second < deap[(pos - 1) / 2].second) {
         std::swap(deap[pos], deap[(pos - 1) / 2]);
         pos = (pos - 1) / 2;
       }
     }
 
     void ReheapUpMax(int pos) {
-      while (pos > 0 && deap[pos].second > deap[(pos - 1) / 2].second) {
+      while (pos > 2 && deap[pos].second > deap[(pos - 1) / 2].second) {
         std::swap(deap[pos], deap[(pos - 1) / 2]);
         pos = (pos - 1) / 2;
       }
@@ -135,16 +107,18 @@ class DEAP {
       deap.push_back(std::make_pair(index, graduates));
       int pos = deap.size() - 1;
       int corPos = findCorrespondingNode(pos);
-      //  Examine the corresponding nodes
-      if (isInMinHeap(pos)) {
-        if (deap[pos].second > deap[corPos].second) {
-          std::swap(deap[pos], deap[corPos]);
-          pos = corPos;
-        }
-      } else {
-        if (deap[pos].second < deap[corPos].second) {
-          std::swap(deap[pos], deap[corPos]);
-          pos = corPos;
+      //  Examine the corresponding nodes (skip if corPos hit the dummy at index 0)
+      if (corPos > 0) {
+        if (isInMinHeap(pos)) {
+          if (deap[pos].second > deap[corPos].second) {
+            std::swap(deap[pos], deap[corPos]);
+            pos = corPos;
+          }
+        } else {
+          if (deap[pos].second < deap[corPos].second) {
+            std::swap(deap[pos], deap[corPos]);
+            pos = corPos;
+          }
         }
       }
       // ReheapUp if necessary
@@ -157,7 +131,7 @@ class DEAP {
 
      std::pair<int, int> getMax() {
       if (!deap.empty()) {
-        return deap[0];
+        return deap[2];
       }
       return std::make_pair(-1, -1);
     }
@@ -235,6 +209,10 @@ class system {
       while (true) {
         std::cout << std::endl << "Input a file number ([0] Quit): ";
         std::cin >> filenumber;
+        if (filenumber == "0") {
+          std::cout << std::endl;
+          return;
+        }
         dataList = IO::readData(filenumber);
         if (dataList.empty()) {
           std::cout << std::endl << "### input" << filenumber << ".txt does not exist! ###" << std::endl;
@@ -261,6 +239,10 @@ class system {
       while (true) {
         std::cout << std::endl << "Input a file number ([0] Quit): ";
         std::cin >> filenumber;
+        if (filenumber == "0") {
+          std::cout << std::endl;
+          return;
+        }
         dataList = IO::readData(filenumber);
         if (dataList.empty()) {
           std::cout << std::endl << "### input" << filenumber << ".txt does not exist! ###" << std::endl;
@@ -298,7 +280,7 @@ class system {
         } else if (choice == "2") {
           mission2();
         } else {
-          std::cout << std::endl << "Command does not exist!" << std::endl;
+          std::cout << std::endl << "Command does not exist!" << std::endl << std::endl;
         }
       }
     }
